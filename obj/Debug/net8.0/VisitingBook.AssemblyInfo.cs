862cfcaf575cfa1c0904d992e0382b15bfd33840
Ayush7303 <ayushrana767@gmail.com>
2024-10-10 17:19:23 +0530

    AyushModel

--- obj/Debug/net8.0/VisitingBook.AssemblyInfo.cs
+++ obj/Debug/net8.0/VisitingBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisitingBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fdf699f7f3780fd85b47ae0fe64d7bb282a5c8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisitingBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisitingBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
