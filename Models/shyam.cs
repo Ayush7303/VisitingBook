1898c204c7498d837e5fdf129acf35fd70d33747
shyam <shyamsadisatsowala123@gmail.com>
2024-10-10 17:36:54 +0530

    Sample

---
+++ Models/shyam.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+ 
+namespace VisitingBook.Models
+{
+    public class shyam
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        
+    }
+}
